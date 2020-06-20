833f825ce2012f4375c2ae3a18418732d6f33eb8
Катя <58567818+2mkat@users.noreply.github.com>
2020-06-21 00:40:49 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,59 +2,58 @@
 #include <string.h>
 #include <stdio.h>
 
-int  parse(stack* head, int sym){
-    int num2 = 0;
-    int x = 0;
-    switch(sym){
-        case ' ':
-            break;
-        case '=':
-            printf("\nResult = %d", stack_pop(head, 0));
-            exit;
-        case '+':
-            num2 = stack_pop(head, 0);
-            printf("%d\n", num2);
-            stack_push(head,0, num2 + stack_pop(head, 0));
-            break;
-        case '-':
-            stack_push(head,0, -stack_pop(head,0) + stack_pop(head, 0));
-            break;
-        case '*':
-            stack_push(head,0, stack_pop(head,0) * stack_pop(head, 0));
-            break;
-        case '/':
-                num2 = stack_pop(head, 0);
-                if (num2 == 0)
-                    printf("error: zero divisor\n");
-                stack_push(head, 0, stack_pop(head, 0) / stack_pop(head, 0));
-                break;
-        default:
-            stack_push(head, 0, sym);
-            break;
+int  parse(stack head, int pos, char sym){
+    int num2 = 0, current;
+    if(pos == 0) {
+        current = 0;
     }
- /* if(sym == 12 || sym == 2){
-      stack_push(head, 0, sym);
-  }
-  if(sym == '+'){
-      stack_push(head, 0, stack_pop(head, 0) + stack_pop(head, 0));
-      printf("%d\n", stack_pop(head, 0));
-  }*/
-  return 0;
+     else{
+         //puts("Before pop");
+         current = stack_pop(head);
+         //puts("After pop");
+     }
+     if(sym >= '0' && sym <= '9'){
+        stack_push(head, current * 10 + sym - 48);
+     }
+      switch(sym) {
+          case '\n':
+              pos = 0;
+              break;
+          case ' ':
+              break;
+          case '+':
+              stack_push(head, stack_pop(head) + stack_pop(head));
+              break;
+          case '-':
+              stack_push(head, -stack_pop(head) + stack_pop(head));
+              break;
+          case '*':
+              stack_push(head,stack_pop(head) * stack_pop(head));
+              break;
+          case '/':
+              num2 = stack_pop(head);
+              if (num2 == 0)
+                  printf("error: zero divisor\n");
+              stack_push(head, stack_pop(head) / stack_pop(head));
+              break;
+          case '=':
+              printf("\nResult = %d", stack_pop(head));
+              exit;
+      }
+    return pos;
 };
 
 int main(){
-    stack* head = calloc(1, sizeof(stack));
-    int cur = 0;
-    char sym = 0;
+    stack head = stack_new();
 
-    while(!feof(stdin)){
-        if(fscanf(stdin, "%d", &cur) != 1){
-            fscanf(stdin, "%c", &sym);
-            parse(head, sym);
-        }
-        else {
-            parse(head, cur);
-        }
-    }
+     int pos = 0;
+     char ch = fgetc(stdin);
+
+     while(ch != EOF) {
+         parse(head, pos, ch);
+         ch = fgetc(stdin);
+         pos++;
+     }
+
     return 0;
 }
