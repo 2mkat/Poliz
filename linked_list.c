59eeb6b6ea5b8da1fbe43212e4669bfa6627c0dc
Катя <58567818+2mkat@users.noreply.github.com>
2020-06-21 15:40:14 +0700

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -3,10 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void insert(linked_list* ll, int pos, int el)
-{
-    for (int i = 0; i < pos; i++)
-    {
+void insert(linked_list* ll, int pos, int el){
+    for (int i = 0; i < pos; i++){
         ll = ll->next;
     }
     linked_list* ll1 = calloc(1, sizeof(linked_list));
@@ -15,18 +13,33 @@ void insert(linked_list* ll, int pos, int el)
     ll->next = ll1;
 };
 
-int delete(linked_list* ll, int pos)
-{
-    for (int i = 0; i < pos; i++)
-    {
+int delete(linked_list* ll, int pos){
+    for (int i = 0; i < pos; i++){
         ll = ll->next;
     }
-puts("lala1");
+//puts("lala1");
     int el1 = ll->next->el;
-puts("lala2");
+//puts("lala2");
     linked_list* ll1 = ll->next;
     ll->next = ll->next->next;
     free(ll1);
 
     return el1;
+};
+
+int len(linked_list* ll){
+    int res=0;
+    while (ll->next != NULL){
+        ll = ll->next;
+        res++;
+    }
+    return res;
+};
+
+void print(linked_list* head){
+    linked_list* next = head->next;
+    while (next != NULL){
+        printf("%d ", next->el);
+        next = next->next;
+    }
 };
